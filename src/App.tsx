d6b0d4ab924f910a674b4e5bb77cea97a495e0dd
unknown <mihalcann@gmail.com>
2025-09-18 23:59:56 +0300

    product-page

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Hom />} />
-      <Route path="/about" element={<h1>О нас</h1>} />
       <Route path="/product/:id" element={<ProductPage />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
